787169124c12437f9a13f058e59dae00d12de4aa
Rahul choudhary <rgodara152001@gmail.com>
2024-07-18 11:10:05 +0530

    code

--- class/index.js
+++ class/index.js
@@ -315,7 +315,6 @@ break;
 
 
  }
- */
 // function licenceChecker(age) {
 //   console.log(`User Input was :-  ${age}`);
 //   if (age > 18 && age <= 60) {
@@ -345,3 +344,161 @@ function loops(n) {
 // loops(10);
 // loops(100);
 loops(150);
+
+
+ 
+
+function questionFourth(starting, ending, sequence) {
+  for (var i = starting; i >= ending; i -= sequence) {
+    console.log(i);
+  }
+}
+
+questionFourth(40, 20, 3);
+
+ 
+
+function reverseOdd(starting, ending) {
+  for (var i = starting; i >= ending; i--) {
+    // console.log(i)
+    if (i % 2 == 1) {
+      console.log(i);
+    }
+    // i--
+  }
+}
+
+reverseOdd(40, 0);
+
+ 
+
+"Even count : 10"
+"Odd count : 14"
+
+function countOddEven(starting, ending) {
+  var evenCount = 0;
+  var oddCount = 0;
+  for (var i = starting; i >= ending; i--) {
+    // console.log(i);
+    if (i % 2 == 0) {
+      // even
+      evenCount++;
+    } else {
+      // odd
+      oddCount++;
+    }
+  }
+  console.log(`Even count : ${evenCount}`);
+  console.log(`Odd count :  ${oddCount}`);
+}
+
+countOddEven(60, 20);
+
+// Q.1 = sum of all numbers from range 10 - 40;
+
+let sum = 0;
+for ( let i = 10; i <= 40 ; i++){
+    sum += i;
+}
+
+console.log(sum);
+
+
+factorial = (n) => {
+  var sum = 1;
+     for(let i = 1 ; i <= n ;  i++) {
+      sum *= i;
+      console.log(sum);
+     }
+}
+
+factorial(8);
+
+var name = 'names';
+
+console.log(name[name.length-1]);
+
+
+function sumOfAllNumbers(starting, ending) {
+  var count = 0; // 10 21 33
+  for (var i = starting; i <= ending; i++) {
+    // console.log(i)
+    // console.log(count, "count", i, "i");
+    count = count + i;
+    // i++
+  }
+  console.log("count :" ,count)
+}
+
+sumOfAllNumbers(10, 40);
+
+
+
+
+
+
+
+
+// Q Calculate factorial of a number
+// 5 * 4 * 3 * 2 * 1 => 120
+
+function findFactorial(number){
+    var count = 1; // 2 6
+    for(var i = 2; i <= number; i++){
+        // console.log(i)
+        count = count * i;
+        // i++ 4
+    }
+    console.log(count)
+}
+
+findFactorial(1)
+
+
+
+
+
+
+// Q Given a string find is string palindrome or not .
+
+function isPalindrome(string) {
+  for (var i = 0; i <= Math.floor((string.length - 1) / 2); i++) {
+    console.log(string[i]);
+  }
+}
+
+isPalindrome("roataor");
+
+// var string = "roataor"
+// console.log(string[Math.floor((string.length - 1) / 2)])
+
+// 0 == 6 , r == r
+// 1 == 5 , o == 0
+// 2 == 4 , a == a
+
+// left side incrementing
+// right side decrementing
+
+
+
+
+
+
+var name = "rotor";
+console.log(name.length -1 ); // its calculates letter from 1 .
+console.log(name[9]) // its calculates letter from 0.
+console.log(name[name.length - 1]);
+console.log(Math.floor(name.length / 2));
+
+
+
+
+
+
+
+
+var number =  536.3242;
+console.log(Math.floor(number))
+console.log(Math.ceil(number))
+
+*/
