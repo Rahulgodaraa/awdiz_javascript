9e5e3aa7ea0b0c093437f79448718790748a4f77
Rahul choudhary <rgodara152001@gmail.com>
2024-07-19 23:08:31 +0530

    assignment

--- class/index.js
+++ class/index.js
@@ -501,4 +501,73 @@ var number =  536.3242;
 console.log(Math.floor(number))
 console.log(Math.ceil(number))
 
+
 */
+
+// Q . Sum of all numbers from range 10 - 40;
+
+sumnums = (s,e) => {
+  let sum = 0;
+  for(var i = s;  i <= e ; i++ ){
+    sum = sum + i;
+  }
+  console.log('Sum of Numbers is :- ' + sum);
+}
+
+sumnums(10,40);
+
+
+// Q Calculate factorial of a number
+factnums = (s) => {
+  let sum = 1;
+  for(var i = 1;  i <= s ; i++ ){
+    sum = sum * i;
+  console.log('Factorial of Numbers ' + i + ' is :- ' + ' ' + sum);
+
+  }
+  console.log('Final Factorial  is :- ' + sum);
+}
+
+factnums(10);
+
+// Q Given a string find is string palindrome or not .
+
+function isPalindrome(string) {
+  for (var i = 0; i <= Math.floor((string.length - 1) / 2); i++) {
+    console.log(string[i]);
+  }
+}
+
+isPalindrome("roataor");
+
+// var string = "roataor"
+// console.log(string[Math.floor((string.length - 1) / 2)])
+
+// 0 == 6 , r == r
+// 1 == 5 , o == 0
+// 2 == 4 , a == a
+
+// left side incrementing
+// right side decrementing
+
+
+
+
+
+
+var name = "rotor";
+console.log(name.length -1 ); // its calculates letter from 1 .
+console.log(name[9]) // its calculates letter from 0.
+console.log(name[name.length - 1]);
+console.log(Math.floor(name.length / 2));
+
+
+
+
+
+
+
+
+var number =  536.3242;
+console.log(Math.floor(number))
+console.log(Math.ceil(number));
