84d0cbd6b7c102c48a3ae3e6585c5ee7ce4c5dd9
Rahul choudhary <rgodara152001@gmail.com>
2024-07-29 20:33:59 +0530

    day1

---
+++ practice/groupprac.js
@@ -0,0 +1,17 @@
+// // Use driving license function here... use numbers as age
+// const students = ["virat","rahul","rohit","ali"]
+// students.forEach((student)=> console.log(student) )
+
+const students = [17, 18, 20, 50, 65];
+const eligible = [];
+
+license = (students) => {
+  students.forEach((student) => {
+    if (student > 18 && student <= 60) {
+      console.log(student);
+    }
+  });
+};
+
+const output = license(students);
+console.log(output);
